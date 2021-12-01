e19896f6794c235590a928b847c290e233190fd9
0x24d <daniel.longfield@tuta.io>
2021-12-01 19:53:52 +0000

    Move day 1's solutions into single function

--- src/day1.py
+++ src/day1.py
@@ -2,35 +2,33 @@
 
 from common import read_file_to_int_list
 
-if __name__ == '__main__':
-    file_lines = read_file_to_int_list(1)
-    # Part 1
+def calculate_sum(num_list, block_size):
     total = 0
-    last_num = 0
-    for i, n in enumerate(file_lines):
-        if i != 0 and n > last_num:
-            total += 1
-        last_num = n
-
-    print(f'Depth measurement increases {total} times')
-
-    # Part 2
-    sum_blocks = 3
-    total = 0
-    for i in range(0, len(file_lines)):
-        if i >= sum_blocks:
-            last_block_start = i - sum_blocks
-            last_block = file_lines[last_block_start : last_block_start + sum_blocks]
+    for i in range(0, len(num_list)):
+        if i >= block_size:
+            last_block_start = i - block_size
+            last_block = num_list[last_block_start : last_block_start + block_size]
             last_sum = sum(last_block, 0)
             
             current_block_start = last_block_start + 1
-            current_block = file_lines[current_block_start : current_block_start + sum_blocks]
+            current_block = num_list[current_block_start : current_block_start + block_size]
             current_sum = sum(current_block, 0)
 
-            if len(last_block) != 3 or len(current_block) != 3:
+            if len(last_block) != block_size or len(current_block) != block_size:
                 break
             if current_sum > last_sum:
                 total += 1
 
+    return total
+
+
+if __name__ == '__main__':
+    file_lines = read_file_to_int_list(1)
+    # Part 1
+    total = calculate_sum(file_lines, 1)
+    print(f'Depth measurement increases {total} times')
+
+    # Part 2
+    total = calculate_sum(file_lines, 3)
     print(f'Depth measurement sums increased {total} times')
 
