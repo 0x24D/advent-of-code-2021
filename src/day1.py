2fddaa7f5e31725e76c7fb6a6aabbb4651df59fe
0x24d <daniel.longfield@tuta.io>
2021-12-01 19:49:09 +0000

    Add day 1 part 2

--- src/day1.py
+++ src/day1.py
@@ -13,3 +13,24 @@ if __name__ == '__main__':
         last_num = n
 
     print(f'Depth measurement increases {total} times')
+
+    # Part 2
+    sum_blocks = 3
+    total = 0
+    for i in range(0, len(file_lines)):
+        if i >= sum_blocks:
+            last_block_start = i - sum_blocks
+            last_block = file_lines[last_block_start : last_block_start + sum_blocks]
+            last_sum = sum(last_block, 0)
+            
+            current_block_start = last_block_start + 1
+            current_block = file_lines[current_block_start : current_block_start + sum_blocks]
+            current_sum = sum(current_block, 0)
+
+            if len(last_block) != 3 or len(current_block) != 3:
+                break
+            if current_sum > last_sum:
+                total += 1
+
+    print(f'Depth measurement sums increased {total} times')
+
