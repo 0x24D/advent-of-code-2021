d40c0208d85309d61a5a88baa7bf74b89a4cda6b
0x24d <daniel.longfield@tuta.io>
2021-12-02 20:23:22 +0000

    Add day 2 part 2

--- src/day2.py
+++ src/day2.py
@@ -17,3 +17,18 @@ if __name__ == '__main__':
 
     print(f'Final horizontal position * final depth = {horizontal_position * depth}')
 
+    # Part 2
+    horizontal_position, depth, aim = 0, 0, 0
+    for line in file_lines:
+        direction, distance = line.split()
+        distance = int(distance)
+        if direction == 'forward':
+            horizontal_position += distance
+            depth += (aim * distance)
+        elif direction == 'down':
+            aim += distance
+        else:
+            aim -= distance
+
+    print(f'Final horizontal position * final depth = {horizontal_position * depth}')
+
