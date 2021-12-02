c4c9a8175d0b2068fb3e74acd8f4e6d1de670111
0x24d <daniel.longfield@tuta.io>
2021-12-02 20:15:04 +0000

    Add day 2 part 1

---
+++ src/day2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from common import read_file_to_str_list
+
+if __name__ == '__main__':
+    file_lines = read_file_to_str_list(2)
+    # Part 1
+    horizontal_position, depth = 0, 0
+    for line in file_lines:
+        direction, distance = line.split()
+        if direction == 'forward':
+            horizontal_position += int(distance)
+        elif direction == 'down':
+            depth += int(distance)
+        else:
+            depth -= int(distance)
+
+    print(f'Final horizontal position * final depth = {horizontal_position * depth}')
+
